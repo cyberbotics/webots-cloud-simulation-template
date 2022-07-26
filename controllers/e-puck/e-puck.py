f6984a27d8bdfe042ea68b06365b4fa00a18f5e6
Yannick Goumaz <61198661+ygoumaz@users.noreply.github.com>
2022-07-26 09:52:38 +0200

    decrease epuck speed

--- controllers/e-puck/e-puck.py
+++ controllers/e-puck/e-puck.py
@@ -53,7 +53,7 @@ while robot.step(timeStep) != -1:
     if sensorsValues[0] > 100:
       speed.append(0.0)
     else:
-      speed.append(5.0)
+      speed.append(3.0)
   
   # set speed values
   leftMotor.setVelocity(speed[0])
